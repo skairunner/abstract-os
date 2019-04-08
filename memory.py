5db9e36cb3607821b2196297abccf607a1d24cdd
Skye Im <skye.im@nyu.edu>
2019-04-08 19:22:30 +0800

    Meta mem: Add more comments

--- memory.py
+++ memory.py
@@ -1,8 +1,21 @@
 import algorithms as algo
 import exceptions
 
+"""
+Simulates physical memory.
+Our model is segmented, with paging, so the smallest op size is 1 frame.
+Memory is stored as a list, and each element is one frame. Data is represented
+as an integer, making it possible to simulate the effects of trampling memory
+due to student error, as well as allowing for more interesting visualization.
+
+PhysicalMemory uses the following methods from algorithms.py:
+    - allocate_page
+    - free_page
+"""
 class PhysicalMemory:
     def __init__(self, framecount):
+        # Creates physical memory with `framecount` frames.
+
         self.framecount = framecount
         self.state = [0 for x in range(framecount)]
         self.freelist = []
@@ -10,12 +23,18 @@ class PhysicalMemory:
         self.free_page = algo.free_page
 
     def get(self, addr):
+        # Get the data at addr
+
         return self.state[addr]
 
     def set(self, addr, data):
+        # Set the data at addr
+
         self.state[addr] = data
 
     def alloc(self, data):
+        # Find an unused frame, set the data and return its address
+
         addr = self.allocate_page(self.freelist)
         try:
             self.state[addr] = data
@@ -23,5 +42,13 @@ class PhysicalMemory:
             raise exceptions.AddressInvalid(f"Hardware error: memory panic @ {addr} (address doesn't exist)")
         return addr
 
+    """
+    Conduct a free operation on physical memory at frame `addr`.
+    The actual bytes are not overwritten, but the free_page method is called.
+    This allows the alloc algorithm to keep track of truly free memory.
+    :param addr: The numerical frame to free.
+    """
     def free(self, addr):
+        # Upon mem free request, calls the free_page method
+
         self.free_page(self.freelist, addr)
