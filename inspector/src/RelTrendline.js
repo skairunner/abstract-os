6862fe44e18e10ac7df662cdec535d1ecef30929
Skye Im <skye.im@nyu.edu>
2019-04-13 20:29:33 +0800

    Feat RelTrendline: Add top/bottom limits

--- inspector/src/RelTrendline.js
+++ inspector/src/RelTrendline.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as d3scale from 'd3-scale';
 import * as d3shape from 'd3-shape';
+import './RelTrendline.css';
 
 /*
 props should include 'data' as an array of values from 0 to 1.
@@ -21,8 +22,10 @@ export default function RelTrendline(props) {
     */
   } = props;
 
-  const scaleX = d3scale.scaleLinear([0, elements], [0, props.width - 2 * padding_w]);
-  const scaleY = d3scale.scaleLinear([0, 1], [props.height - 2 * padding_h, 0]);
+  const limitX = props.width - 2 * padding_w;
+  const limitY = props.height - 2 * padding_h;
+  const scaleX = d3scale.scaleLinear([0, elements], [0, limitX]);
+  const scaleY = d3scale.scaleLinear([0, 1], [limitY, 0]);
   const line = d3shape.line()
     .x((d, i)=> scaleX(i))
     .y((d) => scaleY(d));
@@ -30,6 +33,8 @@ export default function RelTrendline(props) {
   return (
   <svg width={props.width} height={props.height}>
     <g transform={`translate(${padding_w}, ${padding_h})`}>
+      <line class='boundary' x1='0' y1='0' x2={limitX} y2='0' />
+      <line class='boundary' x1='0' y1={limitY} x2={limitX} y2={limitY} />
       <path d={ line(props.data.slice(-10)) } fill='transparent' stroke={stroke} strokeWidth={stroke_width}/>
     </g>
   </svg>
