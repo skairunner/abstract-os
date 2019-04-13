7c0c722150612db602f59ce82ccbada7fd476421
Skye Im <skye.im@nyu.edu>
2019-04-13 19:15:17 +0800

    Meta: Move RWebSocket to own file

--- inspector/src/App.js
+++ inspector/src/App.js
@@ -1,26 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReconnectingWebSocket from 'reconnecting-websocket';
-
-class RWebSocket extends Component {
-  constructor(props) {
-    super(props);
-    this.rws = new ReconnectingWebSocket(props.uri);
-    if (props.onOpen) {
-      this.rws.addEventListener('open', props.onOpen);
-    }
-    if (props.onClose) {
-      this.rws.addEventListener('close', props.onClose);
-    }
-    if (props.onMessage) {
-      this.rws.addEventListener('message', props.onMessage);
-    }
-  }
-
-  render() {
-    return null
-  }
-}
+import RWebSocket from './RWebSocket';
 
 class App extends Component {
   constructor(props) {
