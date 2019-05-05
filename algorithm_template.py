abcf888c53b1ff739e2ca1d18e4c3345b7121bbd
Skye Im <skye.im@nyu.edu>
2019-05-05 22:41:53 +0800

    Feat sim: Change algorithm file

---
+++ algorithm_template.py
@@ -0,0 +1,96 @@
+from processstate import ProcessState
+
+### MEMORY ###
+
+"""
+You can initialize the freelist here, if you want to.
+"""
+def initialize_freelist(freelist, memsize):
+    for x in range(memsize):
+        freelist.append(x)
+
+"""
+Use the freelist to find an unallocated block of memory and return its address.
+"""
+def allocate_memory(freelist):
+    return freelist.pop()
+
+"""
+Update the freelist so you know that you've freed this memory.
+"""
+def free_memory(freelist, addr):
+    freelist.append(addr)
+
+### PAGE MANAGEMENT ###
+
+"""
+What the 'state' param in on_page_created() should look like initially.
+Preferably only use Python built-in data types and list/set/dict.
+"""
+def initialize_pagemanager_state():
+    return []
+
+"""
+Called after a page is placed into memory.
+The intention is for you to update your 'state' to keep track of evictees.
+"""
+def on_page_loaded(pageid, state):
+    state.append(pageid)
+
+"""
+Called when a page's memory is freed, be it by evicting
+or by being deallocated.
+"""
+def on_page_freed(pageid, state):
+    try:
+        state.remove(pageid)
+    except:
+        pass
+
+"""
+When a process wishes to access data in a page, the page must loaded into memory.
+If the page isn't in memory, this is a 'page fault', and the page manager must retrieve
+the page from the disk and put it in memory. If memory is full, the page manager must choose
+a page to evict.
+
+:param pageid: The number of the page to access.
+:param state: Your defined state. Must only use dicts, sets, lists and Python primitives.
+:param evict_page: The function to call to evict a page. evict_page(pageid)
+:param mem_size: How many pages can fit into memory at once.
+"""
+def handle_pagefault(pageid, state, evict_page, mem_used, mem_size):
+    if len(state) < mem_size:  # If there's free memory, just load
+        return
+    # otherwise, evict & load
+    to_evict = state.pop(0)  # FIFO
+    evict_page(to_evict)
+
+### PROCESS SCHEDULING ###
+
+def init_scheduler(scheduler):
+    scheduler.q = []
+    scheduler.last = None
+
+def admit_process(scheduler, process):
+    scheduler.q.append(process)
+
+def pick_process(scheduler):
+    # for i, process in enumerate(scheduler.q):
+    #     if process.state == ProcessState.READY:
+    #         schedule_me = process
+    #         break
+    # return process
+    schedule_me = None
+    index = None
+    for i, process in enumerate(scheduler.q):
+        if process.state == ProcessState.READY:
+            schedule_me = process
+            index = i
+            break
+    if schedule_me is None:
+        return None
+    scheduler.q.append(scheduler.q.pop(index))
+    return schedule_me
+
+def terminate_process(scheduler, process):
+    scheduler.q.remove(process)
--- algorithms.py
+++ algorithms.py
@@ -1,96 +1,30 @@
-from processstate import ProcessState
+import sys
+import os
+from importlib import import_module
 
-### MEMORY ###
+_defaults = import_module('algorithm_template')
+if 'AOS_ALGORITHMS' in os.environ:
+    _algorithms = import_module(os.environ['AOS_ALGORITHMS'])
+else:
+    _algorithms = _defaults
 
-"""
-You can initialize the freelist here, if you want to.
-"""
-def initialize_freelist(freelist, memsize):
-    for x in range(memsize):
-        freelist.append(x)
+ALGO_NAMES = [
+    'initialize_freelist',
+    'allocate_memory',
+    'free_memory',
+    'initialize_pagemanager_state',
+    'on_page_loaded',
+    'on_page_freed',
+    'handle_pagefault',
+    'init_scheduler',
+    'admit_process',
+    'pick_process',
+    'terminate_process'
+]
 
-"""
-Use the freelist to find an unallocated block of memory and return its address.
-"""
-def allocate_memory(freelist):
-    return freelist.pop()
-
-"""
-Update the freelist so you know that you've freed this memory.
-"""
-def free_memory(freelist, addr):
-    freelist.append(addr)
-
-### PAGE MANAGEMENT ###
-
-"""
-What the 'state' param in on_page_created() should look like initially.
-Preferably only use Python built-in data types and list/set/dict.
-"""
-def initialize_pagemanager_state():
-    return []
-
-"""
-Called after a page is placed into memory.
-The intention is for you to update your 'state' to keep track of evictees.
-"""
-def on_page_loaded(pageid, state):
-    state.append(pageid)
-
-"""
-Called when a page's memory is freed, be it by evicting
-or by being deallocated.
-"""
-def on_page_freed(pageid, state):
+_module = sys.modules[__name__]
+for algo in ALGO_NAMES:
     try:
-        state.remove(pageid)
-    except:
-        pass
-
-"""
-When a process wishes to access data in a page, the page must loaded into memory.
-If the page isn't in memory, this is a 'page fault', and the page manager must retrieve
-the page from the disk and put it in memory. If memory is full, the page manager must choose
-a page to evict.
-
-:param pageid: The number of the page to access.
-:param state: Your defined state. Must only use dicts, sets, lists and Python primitives.
-:param evict_page: The function to call to evict a page. evict_page(pageid)
-:param mem_size: How many pages can fit into memory at once.
-"""
-def handle_pagefault(pageid, state, evict_page, mem_used, mem_size):
-    if len(state) < mem_size:  # If there's free memory, just load
-        return
-    # otherwise, evict & load
-    to_evict = state.pop(0)  # FIFO
-    evict_page(to_evict)
-
-### PROCESS SCHEDULING ###
-
-def init_scheduler(scheduler):
-    scheduler.q = []
-    scheduler.last = None
-
-def admit_process(scheduler, process):
-    scheduler.q.append(process)
-
-def pick_process(scheduler):
-    # for i, process in enumerate(scheduler.q):
-    #     if process.state == ProcessState.READY:
-    #         schedule_me = process
-    #         break
-    # return process
-    schedule_me = None
-    index = None
-    for i, process in enumerate(scheduler.q):
-        if process.state == ProcessState.READY:
-            schedule_me = process
-            index = i
-            break
-    if schedule_me is None:
-        return None
-    scheduler.q.append(scheduler.q.pop(index))
-    return schedule_me
-
-def terminate_process(scheduler, process):
-    scheduler.q.remove(process)
+        setattr(_module, algo, getattr(_algorithms, algo))
+    except AttributeError:
+        setattr(_module, algo, getattr(_defaults, algo))
