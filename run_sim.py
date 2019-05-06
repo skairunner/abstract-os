2649e5599d8955491604261bf3393cc3d125e341
Skye Im <skye.im@nyu.edu>
2019-05-07 00:19:30 +0800

    Fix: listen on 0.0.0.0, decrease scenario memory

--- run_sim.py
+++ run_sim.py
@@ -143,6 +143,6 @@ if __name__ == '__main__':
             await websocket.send(sim.serialized(steps))
 
     asyncio.get_event_loop().run_until_complete(
-        websockets.serve(listen, 'localhost', 8765))
+        websockets.serve(listen, '0.0.0.0', 8765))
 
     asyncio.get_event_loop().run_forever()
